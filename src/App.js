5f6c94d22e3c6d545baaeda6e35026330a7fa495
Ajin Varghese <ajin.varghese@contentstack.com>
2021-02-05 14:45:55 +0530

    Added final working project code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,73 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import Meme from "./components/Meme";
+function objectToQueryParams(obj) {
+  const params = Object.entries(obj).map(([key, value]) => `${key}=${value}`);
+  return "?" + params.join("&");
+}
 function App() {
   const [templates, setTemplates] = useState([]);
   const [template, setTemplate] = useState(null);
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+  const [meme, setMeme] = useState(null);
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes").then((resp) =>
       resp.json().then((result) => setTemplates(result.data.memes))
     );
-  });
+  }, []);
+  function setTheTopText(e) {
+    setTopText(e.target.value);
+  }
+  function setTheBottomText(e) {
+    setBottomText(e.target.value);
+  }
+  if (meme) {
+    return (
+      <div>
+        <img src={meme} alt="some meme" />
+      </div>
+    );
+  }
   return (
     <div className="App">
       {template && (
-        <>
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            //creating custom meme logic here
+            const params = {
+              template_id: template.id,
+              text0: topText,
+              text1: bottomText,
+              username: "xzk03017",
+              password: "xzk03017@cndps.com",
+            };
+
+            const response = await fetch(
+              `https://api.imgflip.com/caption_image${objectToQueryParams(
+                params
+              )}`
+            );
+            const data = await response.json();
+            setMeme(data.data.url);
+          }}
+        >
           <h1>Text for the Meme</h1>
           <Meme template={template} />
-        </>
+          <input
+            placeholder="Top Text"
+            value={topText}
+            onChange={setTheTopText}
+          />
+          <input
+            placeholder="Bottom Text"
+            value={bottomText}
+            onChange={setTheBottomText}
+          />
+          <button type="submit">Generate Meme</button>
+        </form>
       )}
       {!template && (
         <>
